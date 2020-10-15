863e63ad7158f55f81bacff79d4e3fb166ef7e9b
draganapajkic <dragana.pajkic.2370@gmail.com>
2020-10-15 15:03:04 +0200

    adding port

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.get('/profile/:id' , (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)}) //ovaj deo se odnosi na rank, odnosno br slika koje smo uneli
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)}) //ovaj deo se odnosi na rank, odnosno br slika koje smo uneli
 
-app.listen(3000, () =>{
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT, () =>{
+    console.log(`app is running on port 3000 ${process.env.PORT}`);
 })
 
 /* Plan for creating API
