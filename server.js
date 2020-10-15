b885f42adb6a0db94cdec05a4e9cbe476ed80595
draganapajkic <dragana.pajkic.2370@gmail.com>
2020-10-15 17:12:36 +0200

    adding db env

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-defined-49395', //ovo je localhost
-      user : 'postgres',
-      password : 'gaga5545454',
-      database : 'smartbrain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
